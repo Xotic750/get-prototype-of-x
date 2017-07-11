3346e0799b54ba76bd500c66111a08c6a89437d8
Xotic750 <xotic750@gmail.com>
2017-07-11 15:55:39 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,93 +1,65 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/get-prototype-of-x"
- * title="Travis status">
- * <img
- * src="https://travis-ci.org/Xotic750/get-prototype-of-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/get-prototype-of-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/get-prototype-of-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/get-prototype-of-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/get-prototype-of-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/get-prototype-of-x" title="npm version">
- * <img src="https://badge.fury.io/js/get-prototype-of-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * Sham for Object.getPrototypeOf.
- *
- * Requires ES3 or above.
- *
- * @version 1.0.0
+ * @file Sham for Object.getPrototypeOf
+ * @version 1.1.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module get-prototype-of-x
  */
 
-/* eslint strict: 1, max-statements: 1 */
+'use strict';
 
-/* global module */
+var isFunction = require('is-function-x');
+var isNull = require('lodash.isnull');
+var toObject = require('to-object-x');
+var gpo = Object.getPrototypeOf;
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var toStringTag = require('to-string-tag-x');
-  var isNull = require('lodash.isnull');
-  var toObject = require('to-object-x');
-  var gpo = Object.getPrototypeOf;
-
-  if (gpo) {
-    try {
-      gpo = gpo(Object) === Object.prototype && gpo;
-    } catch (ignore) {
-      gpo = null;
-    }
+if (gpo) {
+  try {
+    gpo = gpo(Object) === Object.prototype && gpo;
+  } catch (ignore) {
+    gpo = null;
   }
+}
 
-  if (gpo) {
-    try {
-      gpo(1);
-    } catch (ignore) {
-      var $getPrototypeOf = gpo;
-      gpo = function getPrototypeOf(obj) {
-        return $getPrototypeOf(toObject(obj));
-      };
-    }
-  } else {
+if (gpo) {
+  try {
+    gpo(1);
+  } catch (ignore) {
+    var $getPrototypeOf = gpo;
     gpo = function getPrototypeOf(obj) {
-      var object = toObject(obj);
-      var proto = object.__proto__; // eslint-disable-line no-proto
-      if (proto || isNull(proto)) {
-        return proto;
-      } else if (toStringTag(object.constructor) === '[object Function]') {
-        return object.constructor.prototype;
-      } else if (object instanceof Object) {
-        return Object.prototype;
-      } else {
-        return null;
-      }
+      return $getPrototypeOf(toObject(obj));
     };
   }
+} else {
+  gpo = function getPrototypeOf(obj) {
+    var object = toObject(obj);
+    // eslint-disable-next-line no-proto
+    var proto = object.__proto__;
+    if (proto || isNull(proto)) {
+      return proto;
+    }
 
-  /**
-   * This method returns the prototype (i.e. the value of the internal [[Prototype]] property)
-   * of the specified object.
-   *
-   * @param {*} obj The object whose prototype is to be returned.
-   * @return {Object} The prototype of the given object. If there are no inherited properties, null is returned.
-   * @example
-   * var getPrototypeOf = require('get-prototype-of-x');
-   * getPrototypeOf('foo'); // String.prototype
-   */
-  module.exports = gpo;
-}());
+    if (isFunction(object.constructor)) {
+      return object.constructor.prototype;
+    }
+
+    if (object instanceof Object) {
+      return Object.prototype;
+    }
+
+    return null;
+  };
+}
+
+/**
+ * This method returns the prototype (i.e. the value of the internal [[Prototype]] property)
+ * of the specified object.
+ *
+ * @param {*} obj - The object whose prototype is to be returned.
+ * @returns {Object} The prototype of the given object. If there are no inherited properties, null is returned.
+ * @example
+ * var getPrototypeOf = require('get-prototype-of-x');
+ * getPrototypeOf('foo'); // String.prototype
+ */
+module.exports = gpo;
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,71 +1,63 @@
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1 */
+'use strict';
 
-/* global JSON:true, expect, module, require, describe, it, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var getPrototypeOf;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    getPrototypeOf = require('../../index.js');
-  } else {
-    getPrototypeOf = returnExports;
+var getPrototypeOf;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  describe('getPrototypeOf', function () {
-    it('should return the [[Prototype]] of an object', function () {
-      var Foo = function () {};
-      var proto = getPrototypeOf(new Foo());
-      expect(proto).toBe(Foo.prototype);
-    });
-
-    it('should shamone to the `Object.prototype` if `object.constructor` is not a function', function () {
-      var Foo = function () {};
-      Foo.prototype.constructor = 1;
-
-      var proto = getPrototypeOf(new Foo());
-
-      if (proto === Foo.prototype) {
-        expect(proto).toBe(Foo.prototype);
-      } else {
-        expect(proto).toBe(Object.prototype);
-      }
-    });
-
-    it('should throw error for `null` or `undefined`', function () {
-      expect(function () {
-        getPrototypeOf();
-      }).toThrow();
-
-      expect(function () {
-        getPrototypeOf(void 0);
-      }).toThrow();
-
-      expect(function () {
-        getPrototypeOf(null);
-      }).toThrow();
-    });
-
-    it('should work with primitives', function () {
-      expect(getPrototypeOf(1)).toBe(Number.prototype);
-      expect(getPrototypeOf(true)).toBe(Boolean.prototype);
-      expect(getPrototypeOf('')).toBe(String.prototype);
-    });
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
   });
-}());
+  getPrototypeOf = require('../../index.js');
+} else {
+  getPrototypeOf = returnExports;
+}
+
+describe('getPrototypeOf', function () {
+  it('should return the [[Prototype]] of an object', function () {
+    var Foo = function () {};
+    var proto = getPrototypeOf(new Foo());
+    expect(proto).toBe(Foo.prototype);
+  });
+
+  it('should shamone to the `Object.prototype` if `object.constructor` is not a function', function () {
+    var Foo = function () {};
+    Foo.prototype.constructor = 1;
+
+    var proto = getPrototypeOf(new Foo());
+
+    if (proto === Foo.prototype) {
+      expect(proto).toBe(Foo.prototype);
+    } else {
+      expect(proto).toBe(Object.prototype);
+    }
+  });
+
+  it('should throw error for `null` or `undefined`', function () {
+    expect(function () {
+      getPrototypeOf();
+    }).toThrow();
+
+    expect(function () {
+      getPrototypeOf(void 0);
+    }).toThrow();
+
+    expect(function () {
+      getPrototypeOf(null);
+    }).toThrow();
+  });
+
+  it('should work with primitives', function () {
+    expect(getPrototypeOf(1)).toBe(Number.prototype);
+    expect(getPrototypeOf(true)).toBe(Boolean.prototype);
+    expect(getPrototypeOf('')).toBe(String.prototype);
+  });
+});

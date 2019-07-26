86b19a39da284458a93a0a308696d47f94f7fe5e
Graham Fairweather <xotic750@gmail.com>
2019-07-26 16:23:53 +0200

    :bookmark: v2.0.15
    Auto build

--- dist/get-prototype-of-x.js
+++ dist/get-prototype-of-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-25T22:11:37.478Z",
+  "date": "2019-07-26T14:23:46.436Z",
   "describe": "",
   "description": "Sham for Object.getPrototypeOf",
   "file": "get-prototype-of-x.js",
   "hash": "4e15b16333432892f8e0",
   "license": "MIT",
-  "version": "2.0.14"
+  "version": "2.0.15"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
